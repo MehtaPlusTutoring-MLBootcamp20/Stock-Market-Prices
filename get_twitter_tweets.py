b559d83d4d223d5e1c830a70f384ade10d5ee66c
Michelle Zhu <mixue.zhu@gmail.com>
2020-07-16 11:15:41 -0400

    getting tweets code

--- get_twitter_tweets.py
+++ get_twitter_tweets.py
@@ -1,15 +1,7 @@
-import tweepy
-from datetime import timedelta, date
-import numpy as np
+from datetime import timedelta, date, timezone
+import twint
+import pandas as pd
 
-CONSUMER_KEY = "niFysaEmv6CTSrG9pP5IA3QDK"
-CONSUMER_SECRET = "i1MFpIFS4SYYllSDMeis9dCk9WudHGB3bF04HJoUqJfH2nGtt6"
-
-auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-
-api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True, compression = True)
-
-# Get input from user
 response = input("Please enter Keyword: ")
 
 print("Fetch twitter data for "+ response + " company keyword....")
@@ -18,20 +10,40 @@ keyword = response
 
 start = date(2019, 7, 15)
 end = date(2020, 7, 10)
+#start = date(2020,7,8)
+#end = date(2020,7,10)
 delta = end-start
 dateList = []
+#print(delta.days)
 for i in range(delta.days):
-    if (i % 7 != 5) or (i % 7 != 6):
+    if (i % 7 == 0) or (i % 7 == 1) or (i % 7 == 2) or (i % 7 == 3) or (i % 7 == 4):
         new_day = start + timedelta(i)
-        dateList.append(new_day.strftime("%Y-%m-%d"))
+        print(new_day)
+        dateList.append(new_day)
+print(len(dateList))
+c = twint.Config()
+c.Store_object = True
+c.Pandas = True
+c.Search = keyword
+c.Limit = 50
+c.Lang = 'en'
+#c.Store_csv = True
 
-allTweets = []
+df = pd.DataFrame()
 for i in range(len(dateList)-1):
     dayTweets = []
-    results = tweepy.Cursor(api.search, q=keyword, lang="en", since=dateList[i], until=dateList[i+1],
-                        result_type="recent", count = 50).items()
-    for tweet in results:
-        dayTweets.append(tweet.text)
-    numpy_array = np.array(dayTweets)
-    transpose = numpy_array.T
-    allTweets.append(transpose)
+    if (i+1) % 5 == 0:
+        c.Since = str(dateList[i])
+        c.Until = str(dateList[i]+timedelta(1))
+    else:
+        c.Since = str(dateList[i])
+        c.Until = str(dateList[i+1])
+    twint.run.Search(c)
+    Tweets_df = twint.storage.panda.Tweets_df
+    df = pd.concat([df, Tweets_df])
+
+drop_columns = df.drop(columns = ["id", "conversation_id", "created_at", "timezone", "place", "hashtags", "cashtags", "user_id", "user_id_str", "username", "name", "day", "hour", "link", "retweet","nlikes","nreplies","nretweets","quote_url","search","near","geo","source","user_rt_id","user_rt","retweet_id","reply_to","retweet_date", "translate","trans_src", "trans_dest"])
+
+drop_columns['date'] = pd.to_datetime(drop_columns["date"], format='%Y-%m-%d %H:%M:%S').dt.strftime('%Y-%m-%d')
+tweets_df = drop_columns.groupby("date").agg(lambda x: x.tolist())
+tweets_df.to_csv('tweets.csv', index=False)
