989c97b126663891c9e1210a53845e387669195f
Michelle Zhu <mixue.zhu@gmail.com>
2020-07-17 11:06:51 -0400

    getting tweets code more generalizable

--- get_tweets.py
+++ get_tweets.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 start = date(2019, 7, 15)
 end = date(2020, 7, 10)
+#end = date(2019,7,20)
 delta = end-start
 dateList = []
 
@@ -12,7 +13,6 @@ for i in range(delta.days):
     dateList.append(new_day)
 
 dates = pd.DataFrame(dateList, columns = ["Date"])
-#dates.to_csv("dates.csv", index = False)
 
 response = input("Please enter Keyword: ")
 
@@ -43,10 +43,7 @@ drop_columns['date'] = pd.to_datetime(drop_columns["date"], format='%Y-%m-%d %H:
 tweets = drop_columns.groupby("date").agg(lambda x: x.tolist())
 tweets_separate = tweets.tweet.apply(pd.Series)
 
-dates['tmp'] = 1
-tweets_separate['tmp'] = 1
+dates = dates[:-1]
+tweets_df= pd.concat([dates.reset_index(drop=True),tweets_separate.reset_index(drop=True)], axis=1)
 
-tweets_df = pd.merge(dates, tweets_separate, on=['tmp'])
-tweets_df = tweets_df.drop('tmp', axis=1)
-
-tweets_df.to_csv("blue_apron_tweets.csv", index = False)
+tweets_df.to_csv("AMZN.csv", index = False)
